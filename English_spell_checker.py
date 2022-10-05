73e54870a77ecd442de285b5db84b415b6aff15b
siyamfahad <siyamweb3@gmail.com>
2022-10-05 12:17:01 +0600

    first commit

---
+++ English_spell_checker.py
@@ -0,0 +1,12 @@
+from textblob import TextBlob    
+
+t = 1
+while t:
+    a = input("Enter the word to be checked:- ")	 
+    print("original text: "+str(a))     
+
+    b = TextBlob(a)  
+
+    
+    print("corrected text: "+str(b.correct()))
+    t = int(input("Try Again? 1 : 0 "))
---
+++ README.md
@@ -0,0 +1,7 @@
+🎈English Spell Checker 
+Here, you can input any word and check if it is having a correct spelling or not.
+
+✔✔✔ Prerequisites
+First thing which you need to install is textblob library
+>pip install textblob
+
